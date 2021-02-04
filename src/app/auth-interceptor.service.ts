12addc6937913c567a1fb82c6c6b98bbb4634348
alvalle09 <phredvalle@gmail.com>
2021-02-03 23:46:11 -0600

    create a leaner interceptor

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -3,17 +3,9 @@ import { HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from "@angul
 
 export class AuthInterCeptorService implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler) {
-        console.log('Request on its way!');
-        console.log(req.url);
         const modifiedRequest = req.clone({headers: req.headers.append('Auth', 'xyz')});
 
         // handle the modifiedRequest, not the oringal request
-        return next.handle(modifiedRequest).pipe(tap(event => {
-            console.log(event);
-            if (event.type === HttpEventType.Response) {
-                console.log('Response arrived, body data: ');
-                console.log(event.body);
-            }
-        }));
+        return next.handle(modifiedRequest);
     }
 }
